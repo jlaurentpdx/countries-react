3bc2c4e4be8c655ddc2b85108cf5a4cd47833acc
Jordan Laurent <jlaurent.pdx@gmail.com>
2021-12-07 20:47:50 -0800

    all country flags rendering

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   const [loading, setLoading] = useState(true); // Initialize loading state to true
   const [countries, setCountries] = useState([]); // Initialize countries to empty array
-  const [continent, setContinents] = useState('All'); // Initialize Continents to 'All'
+  const [continent, setContinent] = useState('All'); // Initialize Continents to 'All'
   const [query, setQuery] = useState(''); // Initialize to empty string
 
   useEffect(() => {
@@ -13,6 +13,7 @@ function App() {
       const data = await getCountries();
       setCountries(data);
       setLoading(false);
+      console.log(data);
     };
     fetchData();
   }, []);
@@ -21,8 +22,7 @@ function App() {
 
   function filterCountries() {
     return countries.filter((country) => {
-      country.name.includes(query.value) &&
-        (country.continent === continent || continent === 'All');
+      country.name.includes(query) && (country.continent === continent || continent === 'All');
     });
   }
 
@@ -34,8 +34,21 @@ function App() {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
+      <select value={continent} onChange={(e) => setContinent(e.target.value)}>
+        <option value="All">All</option>
+        <option value="Africa">Africa</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="North America">North America</option>
+        <option value="Oceania">Oceania</option>
+        <option value="South America">South America</option>
+      </select>
       {countries.map((country) => (
-        <p key={country.id}>{country.name}</p>
+        <p key={country.id}>
+          <img src={`https://flagcdn.com/16x12/${country.iso2.toLowerCase()}.png`} />
+          {country.name}
+        </p>
       ))}
     </div>
   );
